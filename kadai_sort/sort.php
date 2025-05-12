fd3edfc2bdc2c0a0aac72fb9b5faddbe6d0e1dec
MomokoMori <momourimomo@gmail.com>
2025-05-12 16:29:39 +0900

    kadai_sortを追加
    kadai_sortを追加

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,22 +9,22 @@
 <body>
     <p>
         <?php
-         function sort_2way($array, $order) {
-          if ($order === 'TRUE') {
-          echo '昇順にソートします。<br>';
-          sort ($array);
-          } else {
-          echo '降順にソートします。<br>';
-          rsort ($array);}
-          foreach ($array as $num) {
-          echo $num . '<br>';
-          }
+        function sort_2way($array, $order) {
+        if ($order) {
+        echo '昇順にソートします。<br>';
+        sort ($array);
+        } else {
+        echo '降順にソートします。<br>';
+        rsort ($array);}
+        foreach ($array as $num) {
+        echo $num . '<br>';
+        }
         }
         
         $nums = [15, 4, 18, 23, 10];
         
-        sort_2way($nums, 'TRUE');
-        sort_2way($nums, 'FALSE');
+        sort_2way($nums, TRUE);
+        sort_2way($nums, FALSE);
         ?>
     </p>
 </body>
